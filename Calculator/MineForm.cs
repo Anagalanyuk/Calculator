ec34f695b63e41d1c07d65a6ffad252df9e60a2f
Anatoliy Anatoliy <anagalanyuk@gmail.com>
2018-05-25 09:43:23 +0300

    day 1

--- Calculator/MineForm.cs
+++ Calculator/MineForm.cs
@@ -10,11 +10,21 @@ using System.Windows.Forms;
 
 namespace Calculator
 {
-	public partial class Form1 : Form
+	public partial class MineForm : Form
 	{
-		public Form1()
+		public MineForm()
 		{
 			InitializeComponent();
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+
 		}
 	}
 }
